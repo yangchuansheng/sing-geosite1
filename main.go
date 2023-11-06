7bc6cd2ff5d27d2259eb2fd78a94c9d7b2e8ffef
Carson Yang <yangchuansheng33@gmail.com>
2023-11-06 15:00:34 +0800

    Update main.go

--- main.go
+++ main.go
@@ -234,7 +234,7 @@ func release(source string, destination string, output string) error {
 }
 
 func main() {
-	err := release("Loyalsoldier/v2ray-rules-dat", "1715173329/sing-geosite", "geosite.db")
+	err := release("yangchuansheng/domain-list-custom", "yangchuansheng/sing-geosite", "geosite.db")
 	if err != nil {
 		logrus.Fatal(err)
 	}
